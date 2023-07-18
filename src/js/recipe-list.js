6efc4f7210d45f1cab1c2fbdd4bed0d42d733d08
Tim <tomlinsont@byui.edu>
2023-07-18 14:05:06 -0600

    got responce on search.js

--- src/js/recipe-list.js
+++ src/js/recipe-list.js
@@ -1,7 +1,8 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import recipeList from "./recipeList.mjs";
 
 loadHeaderFooter();
 
+const category = getParam("category");
 recipeList(".recipe-list", "chicken");
 
